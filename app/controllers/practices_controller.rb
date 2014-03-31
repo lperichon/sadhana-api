c2dd782962b07b95002694452336d07ff3a7be95
Luis Perichon <luisperichon@gmail.com>
2014-03-30 22:38:32 -0300

    create practice

--- app/controllers/practices_controller.rb
+++ app/controllers/practices_controller.rb
@@ -7,4 +7,19 @@ class PracticesController < ApplicationController
     practice = Practice.find(params[:id])
     render json: practice
   end
+
+  def create
+    practice = Practice.new(practice_params)
+
+    if practice.save
+      render json: practice, status: :created
+    else
+      render json: practice.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def practice_params
+    params.require(:practice).permit(:name, :description)
+  end
 end
