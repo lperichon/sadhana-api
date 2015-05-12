147a4d41dae35ef45180ff35c24ad999cffc949e
Luis Perichon <luisperichon@gmail.com>
2015-05-12 18:43:07 -0300

    add continuous & delay attributes to practice

--- app/serializers/practice_serializer.rb
+++ app/serializers/practice_serializer.rb
@@ -1,6 +1,8 @@
 class PracticeSerializer < ActiveModel::Serializer
   attributes :id,
   			 :name,
-  			 :description
+  			 :description,
+  			 :continuous,
+  			 :delay
   has_many :practice_parts
 end
